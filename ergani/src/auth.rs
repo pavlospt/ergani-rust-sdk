46d4360fbd49ae0b264aca41b951b198ce7c2068
Pavlos-Petros Tournaris <p.tournaris@gmail.com>
2025-01-20 18:04:24 +0200

    Start keeping more info when authentication is successful

--- ergani/src/auth.rs
+++ ergani/src/auth.rs
@@ -1,20 +1,32 @@
 use crate::api_error::{APIError, ErganiError};
 use crate::endpoint::AUTHENTICATION_ENDPOINT;
 use anyhow::{bail, Result};
+use bon::Builder;
+use chrono::{DateTime, Utc};
 use reqwest::header;
 use reqwest::header::HeaderMap;
 use serde_json::json;
 
 /// Authentication handler for the Ergani API
-#[derive(Clone)]
+#[allow(dead_code)]
+#[derive(Clone, Builder)]
 pub struct ErganiAuthentication {
-    pub access_token: String,
+    access_token: String,
+    access_token_expired: i64,
+    refresh_token: String,
+    refresh_token_expired: DateTime<Utc>,
 }
 
 #[derive(serde::Deserialize)]
 struct AuthenticationResponse {
     #[serde(rename = "accessToken")]
     access_token: String,
+    #[serde(rename = "accessTokenExpired")]
+    access_token_expired: i64,
+    #[serde(rename = "refreshToken")]
+    refresh_token: String,
+    #[serde(rename = "refreshTokenExpired")]
+    refresh_token_expired: DateTime<Utc>,
 }
 
 impl ErganiAuthentication {
@@ -48,9 +60,14 @@ impl ErganiAuthentication {
             let authentication_response: AuthenticationResponse =
                 response.json::<AuthenticationResponse>().await?;
 
-            Ok(ErganiAuthentication {
-                access_token: authentication_response.access_token,
-            })
+            let authentication = ErganiAuthentication::builder()
+                .access_token(authentication_response.access_token)
+                .access_token_expired(authentication_response.access_token_expired)
+                .refresh_token(authentication_response.refresh_token)
+                .refresh_token_expired(authentication_response.refresh_token_expired)
+                .build();
+
+            Ok(authentication)
         } else {
             let original_error = response.error_for_status_ref().unwrap_err();
             let error_text = response.text().await?.to_string();
